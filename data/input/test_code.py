66803bda7618ecd95f6799d44210d7716dba535f
fhaih379590-cell <grok1324545@outlook.com>
2026-04-28 10:27:23 +0800

    Create test_code.py

---
+++ data/input/test_code.py
@@ -0,0 +1,15 @@
+# 这是一段劣质代码：变量命名模糊，有硬编码，逻辑混乱
+import os
+
+def check():
+    ip = "192.168.1.105" # 硬编码IP
+    key = "secret_password_123" # 敏感信息
+    list = [1,2,3,4,5]
+    for i in list:
+        if i % 2 == 0:
+            print("even")
+        else:
+            print("odd")
+    return True
+
+check()
